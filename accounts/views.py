fa71563323dbf896c572f12dfc979f5a5dfd063d
Vadim <bardiervadim97@gmail.com>
2020-06-13 21:39:54 +0300

    'finish_registration_add_update_user'

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from accounts.forms import UserLoginForm
-
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
 
 # Create your views here.
+User = get_user_model()
 
 
 def login_view(request):
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
         data = form.cleaned_data
-        print('1')
         email = data.get('email')
-        print(email)
         password = data.get('password')
-        print(password)
         user = authenticate(request, email=email, password=password)
         login(request, user)
         return redirect('index')
@@ -23,4 +20,42 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
+    return redirect('index')
+
+
+def register_view(request):
+    form = UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        return render(request, 'accounts/register_done.html', {'new_user': new_user})
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def update_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = UserUpdateForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.is_subscribed = data['is_subscribed']
+                user.save()
+                return redirect('update')
+        form = UserUpdateForm(
+            initial={'city': user.city, 'language': user.language, 'is_subscribed': user.is_subscribed})
+        return render(request, 'accounts/update.html', {'form': form})
+    else:
+        return redirect('login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            qs = User.objects.get(pk=user.pk)
+            qs.delete()
     return redirect('index')
